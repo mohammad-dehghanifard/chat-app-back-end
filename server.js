9b8549067d317a3f8d3bda5bafaf7af902e4906e
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-12-12 00:04:48 -0800

    Implementation of the ability to send text messages on the server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const { log } = require("console");
 const express = require('express');
 const http = require('http');
 const app = express();
@@ -11,6 +12,20 @@ const port = process.env.Port || 3000;
 // Socket Io
 io.on("connection", socket => {
     const socketId = socket.id
+    const userId = socket.handshake.query.userId;
+    console.log(userId);
+
+
+    //emit sendMessage
+    socket.on("send-message",(event) => {
+        console.log(`user ${userId} sending ${event.to} is message ${event.message}`);
+    })
+
+    // disconnect
+    socket.on("disconnect",(event) => {
+        console.log(`user ${userId} disconnected`);
+    })
+
 })
 
 // server listening on port
