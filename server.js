f5e891d36ca1818dfb7531c699bcaf80700faaea
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-12-12 23:13:17 -0800

    Implementation of the ability to send messages between two users

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-const { log } = require("console");
 const express = require('express');
 const http = require('http');
 const app = express();
@@ -8,22 +7,33 @@ const io = require("socket.io")(server);
 
 // variables
 const port = process.env.Port || 3000;
+const users = [];
+
+// Events
+const receivedSingleMessageEvent = process.env.receivedSingleMessageEvent;
 
 // Socket Io
 io.on("connection", socket => {
     const socketId = socket.id
     const userId = socket.handshake.query.userId;
-    console.log(userId);
-
-
+    users.push({ userId: userId, socketId: socketId });
     //emit sendMessage
     socket.on("send-message",(event) => {
-        console.log(`user ${userId} sending ${event.to} is message ${event.message}`);
+      // ارسال پیام بین کاربران
+      const filterUser = users.filter((user) => user.userId === event.to);
+      const receivedSocketId = filterUser[0].socketId;
+      socket.broadcast.to(receivedSocketId).emit(receivedSingleMessageEvent,{
+        "on" : event.on,
+        "message" : event.message
+      })
     })
 
     // disconnect
     socket.on("disconnect",(event) => {
-        console.log(`user ${userId} disconnected`);
+        console.log('\x1b[36m%s\x1b[0m',`user ${userId} disconnected`);
+        const filterUser = users.filter((user) => user.userId === userId);    
+        const user = filterUser[0];
+        users.slice(user,1);
     })
 
 })
