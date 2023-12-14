f630892b23055fee262084de92fb94686a2ca67f
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-12-13 23:43:26 -0800

    impl register function in auth controller

--- controller/auth_cotroller.js
+++ controller/auth_cotroller.js
@@ -1,38 +1,45 @@
-exports.registerUser = (req,res) => {
+const {MongoClient} = require("mongodb");
+
+exports.registerUser = async (req,res) => {
     {
-        const fullName = req.body.fullName
-        const userName = req.body.userName
-        const password = req.body.password
-      
-        if(fullName === undefined || userName === undefined || password === undefined){
+        const userObj = {fullName : req.body.fullName,userName: req.body.userName,password : req.body.password}
+        // clinet no send all properties in the request
+        if(userObj.fullName === undefined || userObj.userName === undefined || userObj.password === undefined){
           res.status(400).json({
               "message": "please enter all required fields",
           })
-      
-          // connect to database
-          MongoClient.connect(process.env.DataBaseUrl,(err,db) => {
-            if(err) console.log(err);
-            // database object
-            const dbo = db.db(process.env.DataBaseName)
-            // user object
-            const newUser = {
-              fullName: fullName,
-              userName: userName,
-              password: password
-            }
-            // create collection
-            dbo.collection(process.env.UsereCollection).insertOne(newUser,(err,result) => {
-              if(err) console.log(err);
-              res.json(
-                {
-                  "message" : "registered successfully",
-                  "data" : result
-                }
-              )
-              db.close();
-            })
-          })
-      
         }
+        // insert user successfully
+        else{
+
+            try{
+            // connect to db
+            const client = new MongoClient(process.env.DataBaseUrl);
+            const db = client.db(process.env.DataBaseName);
+            const userCollection = db.collection(process.env.UsereCollection);
+
+            const newUser = {
+                fullName: userObj.fullName,
+                userName: userObj.userName,
+                password : userObj.password
+            };
+
+            const result = await userCollection.insertOne(newUser)
+
+            res.status(200).json({
+                "message": "user registered successfully",
+                "data": result
+            });
+
+            await client.close;
+
+            } catch(err){
+                console.log(err);
+            }
+        } 
       }
+}
+
+async function openDatabase(collectionName){
+    
 }
