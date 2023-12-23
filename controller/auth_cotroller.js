50f61041fcbd4b817ca4dd22454ece8e7abc1e9f
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-12-22 23:41:29 -0800

    Prevent user registration with duplicate username

--- controller/auth_cotroller.js
+++ controller/auth_cotroller.js
@@ -16,13 +16,24 @@ exports.registerUser = async (req,res) => {
             const db = connectToMongoo(process.env.UsereCollection);
             // create user
             const newUser = {fullName,userName, password};
+            // check username already exists
+            const checkUserName = await db.collection.findOne({userName : userName})
             // insert user
+            if(checkUserName === null)
+            {
             const result = await db.collection.insertOne(newUser)
-
             res.status(200).json({
                 "message": "user registered successfully",
                 "data": result
             });
+            } 
+            // send user allready exists error
+            else {
+                res.status(400).json({
+                    "message": "username already exists please select more username",
+                    "error": "username already exists"
+                });
+            }
             // colose database connection
             await db.client.close;
 
