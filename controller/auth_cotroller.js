593b75cbe3b2f72d08565a872b218334bd9e789e
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-12-23 04:04:23 -0800

    impl signup

--- controller/auth_cotroller.js
+++ controller/auth_cotroller.js
@@ -1,52 +1,117 @@
-const {connectToMongoo} = require("../utils/connect_to_mongoo");
-const bycript = require("bcrypt")
+const { connectToMongoo } = require('../utils/connect_to_mongoo')
+const bcrypt = require('bcrypt')
 
-exports.registerUser = async (req,res) => {
-    {
-        const userObj = {fullName,userName,password} = req.body;
-        // clinet no send all properties in the request
-        if(userObj.fullName === undefined || userObj.userName === undefined || userObj.password === undefined){
-          res.status(400).json({
-              "message": "please enter all required fields",
+exports.registerUser = async (req, res) => {
+  {
+    const userObj = ({ fullName, username, password } = req.body)
+    // clinet no send all properties in the request
+    if (
+      userObj.fullName === undefined ||
+      userObj.username === undefined ||
+      userObj.password === undefined
+    ) {
+      return res.status(400).json({
+        message: 'please enter all required fields'
+      })
+    }
+    // insert user
+    else {
+      try {
+        // connect to db
+        const db = connectToMongoo(process.env.UsereCollection)
+        // hash password
+        const hashPassword = await bcrypt.hash(password, 10)
+        // create user
+        const newUser = {
+          fullName: fullName,
+          username: username,
+          password: hashPassword
+        }
+        // check username already exists
+        const existUser = await db.collection.findOne({ username: username })
+        // insert user
+        if (existUser === null) {
+          const result = await db.collection.insertOne(newUser)
+          res.status(200).json({
+            message: 'user registered successfully',
+            data: result
           })
         }
-        // insert user successfully
-        else{
-            try{
-            // connect to db
-            const db = connectToMongoo(process.env.UsereCollection);
-            // hash password
-            const hashPassword = await bycript.hash(password,process.env.SaltRounds);
-            // create user 
-            const newUser = {fullName,userName, hashPassword};
-            // check username already exists
-            const checkUserName = await db.collection.findOne({userName : userName})
-            // insert user
-            if(checkUserName === null)
-            {
-            const result = await db.collection.insertOne(newUser)
-            res.status(200).json({
-                "message": "user registered successfully",
-                "data": result
-            });
-            } 
-            // send user allready exists error
-            else {
-                res.status(400).json({
-                    "message": "username already exists please select more username",
-                    "error": "username already exists"
-                });
-            }
-            // colose database connection
-            await db.client.close;
-
-            } catch(err){
-                res.status(500).json({
-                    message: "Internal server error",
-                    "error" : err.message
-                });
-            }
-        } 
+        // send user allready exists error
+        else {
+          res.status(400).json({
+            message: 'username already exists please select more username',
+            error: 'username already exists'
+          })
+        }
+        // colose database connection
+        db.client.close
+      } catch (err) {
+        res.status(500).json({
+          message: 'Internal server error',
+          error: err.message
+        })
       }
+    }
+  }
 }
 
+exports.signup = async (req, res) => {
+  try {
+    // get inputs
+    const inputs = ({ username, password } = req.body)
+    // validate inputs
+    if (!inputs.username || !inputs.password) {
+      res.status(400).json({
+        message: 'please enter all required fields'
+      })
+      return
+    }
+    // connect to db
+    const db = connectToMongoo(process.env.UsereCollection)
+    // find user
+    const user = await db.collection.findOne({ username: inputs.username })
+
+    if (user) {
+      // decode password and check
+      const matchPassword = await bcrypt.compare(
+        inputs.password,
+        user.password
+      )
+
+      // match password
+      if(matchPassword){
+        // genrate jwt token....
+
+        res.status(200).json({
+            message: 'sign up successfully',
+            data: user
+          })
+          db.client.close;
+      }
+      // password not match 
+      else{
+        res.status(400).json({
+            message: 'The password entered is not correct',
+            error: 'password invalid'
+          })
+      }
+
+      
+    }
+    // user not found
+     else{
+    
+        res.status(400).json({
+            message: 'user not found',
+            error: 'invalid username'
+          })
+    }
+
+  } catch (err) {
+    res.status(500).json({
+      message: 'Internal server error',
+      error: err.message
+    })
+  }
+}
--- routers/auth.js
+++ routers/auth.js
@@ -3,6 +3,7 @@ const controller = require('../controller/auth_cotroller')
 
 const router = express.Router()
 
-router.post("/register",controller.registerUser);
+router.post("/register",controller.registerUser)
+router.post("/signup",controller.signup)
 
 module.exports = router
